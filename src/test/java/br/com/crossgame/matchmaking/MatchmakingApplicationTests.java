4f7a9d651246380dd13960ed50f10fe6e1927d97
Gabriel Mello <Gabriel.mmello@sptech.school>
2023-02-17 19:42:07 -0300

    wip

---
+++ src/test/java/br/com/crossgame/matchmaking/MatchmakingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crossgame.matchmaking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchmakingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
