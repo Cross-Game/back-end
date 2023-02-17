b95aaa32060f5b593458755b4f638b986b9145b4
Gabriel Mello <Gabriel.mmello@sptech.school>
2023-02-17 19:09:18 -0300

    wip

--- matchmaking/src/main/java/br/com/crossgame/matchmaking/MatchmakingApplication.java
+++ matchmaking/src/main/java/br/com/crossgame/matchmaking/MatchmakingApplication.java
@@ -8,6 +8,7 @@ public class MatchmakingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MatchmakingApplication.class, args);
+		System.out.println("hello word");
 	}
 
 }
