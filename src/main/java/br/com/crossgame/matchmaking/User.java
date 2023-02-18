a76443ab8e3dfe0e6071cf81048fa0d006977f76
Matheus <matheus.patricio@sptech.school>
2023-02-18 15:04:13 -0300

    wip

---
+++ src/main/java/br/com/crossgame/matchmaking/User.java
@@ -0,0 +1,11 @@
+package br.com.crossgame.matchmaking;
+
+public class User {
+    private String name;
+
+    public void validarName(){
+        if(name.equals("aoba")){
+            return;
+        }
+    }
+}
