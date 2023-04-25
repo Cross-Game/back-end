1c67afe346a5e240acbf0ef815754dba77b68f9b
LeonardoMS07 <leonardo.soares@sptech.school>
2023-04-25 19:56:06 -0300

    wip

--- cross-game/src/main/java/br/com/crossgame/matchmaking/api/usecase/OrderListByName.java
+++ cross-game/src/main/java/br/com/crossgame/matchmaking/api/usecase/OrderListByName.java
@@ -1,9 +1,11 @@
 package br.com.crossgame.matchmaking.api.usecase;
 
 import br.com.crossgame.matchmaking.internal.entity.Friends;
+import br.com.crossgame.matchmaking.internal.entity.User;
 
 import java.util.List;
 
 public interface OrderListByName {
     List<Friends> execute(List<Friends> friends);
+    List<User> executeUser(List<User> users);
 }
