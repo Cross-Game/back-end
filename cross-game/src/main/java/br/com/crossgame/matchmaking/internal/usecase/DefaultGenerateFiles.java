acd11f3f872095a864f2f4ac3636d510611173a5
Matheus <matheus.patricio@sptech.school>
2023-04-26 08:31:33 -0300

    add path default

--- cross-game/src/main/java/br/com/crossgame/matchmaking/internal/usecase/DefaultGenerateFiles.java
+++ cross-game/src/main/java/br/com/crossgame/matchmaking/internal/usecase/DefaultGenerateFiles.java
@@ -17,6 +17,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -26,40 +27,47 @@ import java.util.Optional;
 public class DefaultGenerateFiles implements GenerateFiles {
 
     private UserRepository userRepository;
-    @Override
-    public void execute(Long userId,String archiveType) throws IOException {
-      if (!(archiveType.equals("csv") || archiveType.equals("txt"))){
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"Tipo de arquivo inválido!");
-      }
-        User user = userRepository.findById(userId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,String.format("Usuário com o id %s não foi encontrado!",userId)));
-        List<Friends> friends = user.getFriends();
-        if(friends.isEmpty()){
-             throw new ListIsEmptyExcepetion("A lista de amigos está vazia");
-        }
-        ListaObj<Friends> friendsListaObj = new ListaObj<>(friends.size());
-         friends.forEach(friends1 -> friendsListaObj.adiciona(friends1));
 
-        FileWriter writer = new FileWriter("friend-list."+archiveType);
-        writer.append("USERNAME");
-        writer.append(";");
-        writer.append("INICIO AMIZADE");
-        writer.append(";");
-        writer.append("\n");
-
-        for (int i = 0 ; i < friendsListaObj.getNroElem(); i++){
-            Friends friend = friendsListaObj.getElemento(i);
-            try {
-                writer.append(friend.getUsername());
-                writer.append(";");
-                writer.append(friend.getFriendshipStartDate().toString());
-                writer.append(";");
-                writer.append("\n");
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        @Override
+        public void execute(Long userId, String archiveType) throws IOException {
+            if (!(archiveType.equals("csv") || archiveType.equals("txt"))) {
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tipo de arquivo inválido!");
             }
-        }
-        writer.flush();
-        writer.close();
+            User user = userRepository.findById(userId)
+                    .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                            String.format("Usuário com o id %s não foi encontrado!", userId)));
+            List<Friends> friends = user.getFriends();
+            if (friends.isEmpty()) {
+                throw new ListIsEmptyExcepetion("A lista de amigos está vazia");
+            }
+            ListaObj<Friends> friendsListaObj = new ListaObj<>(friends.size());
+            friends.forEach(friends1 -> friendsListaObj.adiciona(friends1));
 
-    }
+            String path = System.getProperty("user.home") + File.separator + "Downloads" + File.separator + "friend-list."
+                    + archiveType;
+            FileWriter writer = new FileWriter(path);
+            writer.append("USERNAME");
+            writer.append(";");
+            writer.append("INICIO AMIZADE");
+            writer.append(";");
+            writer.append("\n");
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            for (int i = 0; i < friendsListaObj.getNroElem(); i++) {
+                Friends friend = friendsListaObj.getElemento(i);
+                try {
+                    writer.append(friend.getUsername());
+                    writer.append(";");
+                    writer.append(friend.getFriendshipStartDate().format(formatter));
+                    writer.append(";");
+                    writer.append("\n");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            writer.flush();
+            writer.close();
+        }
+
 }
