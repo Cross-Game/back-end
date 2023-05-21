8351ca4c0e87d7c61377b8b73dfe6145b35e5d88
Lucas Evangelista <lucas.silva@sptech.school>
2023-05-21 13:31:30 -0300

    Acrescentado anotações na classe

--- cross-game/src/main/java/br/com/crossgame/matchmaking/internal/usecase/DefaultUpdatePasswordByUsernameEmailForLoginServices.java
+++ cross-game/src/main/java/br/com/crossgame/matchmaking/internal/usecase/DefaultUpdatePasswordByUsernameEmailForLoginServices.java
@@ -6,8 +6,12 @@ import br.com.crossgame.matchmaking.api.usecase.RetrieveUserById;
 import br.com.crossgame.matchmaking.api.usecase.UpdatePasswordByUsernameEmailForLoginServices;
 import br.com.crossgame.matchmaking.internal.entity.User;
 import br.com.crossgame.matchmaking.internal.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@AllArgsConstructor
 public class DefaultUpdatePasswordByUsernameEmailForLoginServices
         implements UpdatePasswordByUsernameEmailForLoginServices {
 
