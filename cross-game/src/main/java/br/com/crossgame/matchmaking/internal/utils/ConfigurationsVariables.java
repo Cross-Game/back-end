a27c4f59649b9cde7c4b3facf58283c7860184ff
Matheus <matheus.patricio@sptech.school>
2023-06-10 11:31:54 -0300

    token add

--- cross-game/src/main/java/br/com/crossgame/matchmaking/internal/utils/ConfigurationsVariables.java
+++ cross-game/src/main/java/br/com/crossgame/matchmaking/internal/utils/ConfigurationsVariables.java
@@ -1,7 +1,7 @@
 package br.com.crossgame.matchmaking.internal.utils;
 
 public class ConfigurationsVariables {
-    public static final String API_KEY = "RGAPI-9e5b8900-4d65-45f2-af0c-5d5ec2dce3d7";
+    public static final String API_KEY = "RGAPI-1a0383c5-a608-45d5-b48a-bab49a6044f6";
 
 
 }
