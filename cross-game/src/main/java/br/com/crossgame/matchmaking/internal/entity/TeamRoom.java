00400059d15e229d8f95d1e52a87f63ecd3a0d48
LeonardoMS07 <leonardo.soares@sptech.school>
2023-06-12 22:44:25 -0300

    wip

--- cross-game/src/main/java/br/com/crossgame/matchmaking/internal/entity/TeamRoom.java
+++ cross-game/src/main/java/br/com/crossgame/matchmaking/internal/entity/TeamRoom.java
@@ -15,7 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 public class TeamRoom {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
     private String roomName;
