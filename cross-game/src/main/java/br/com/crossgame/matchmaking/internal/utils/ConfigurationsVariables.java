0c8919ef2c529e3636650842eb088d00ff27b65f
LucasEva <lucas.silva@alpe.com.br>
2023-06-15 00:56:29 -0300

    token riot

--- cross-game/src/main/java/br/com/crossgame/matchmaking/internal/utils/ConfigurationsVariables.java
+++ cross-game/src/main/java/br/com/crossgame/matchmaking/internal/utils/ConfigurationsVariables.java
@@ -1,5 +1,5 @@
 package br.com.crossgame.matchmaking.internal.utils;
 
 public class ConfigurationsVariables {
-    public static final String API_KEY = "RGAPI-f60eb03d-5d1b-43c8-959d-b7043091d1ea";
+    public static final String API_KEY = "RGAPI-e1ea1ff8-1f51-4175-bbc8-d3f04342fbe8";
 }
