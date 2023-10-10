74b14e1408dccd68886725e0a75f953a0a65d02f
Matheus <matheus.patricio@sptech.school>
2023-10-09 22:04:19 -0300

    add user repository update

--- cross-game/src/main/java/br/com/crossgame/matchmaking/internal/usecase/DefaultDeletePreferenceForUserByName.java
+++ cross-game/src/main/java/br/com/crossgame/matchmaking/internal/usecase/DefaultDeletePreferenceForUserByName.java
@@ -5,6 +5,7 @@ import br.com.crossgame.matchmaking.api.usecase.RetrieveUserById;
 import br.com.crossgame.matchmaking.internal.entity.Preference;
 import br.com.crossgame.matchmaking.internal.entity.User;
 import br.com.crossgame.matchmaking.internal.repository.PreferenceRepository;
+import br.com.crossgame.matchmaking.internal.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -18,18 +19,19 @@ import javax.transaction.Transactional;
 public class DefaultDeletePreferenceForUserByName  implements DeletePreferenceForUserByName {
     private PreferenceRepository preferenceRepository;
     private RetrieveUserById retrieveUserById;
+    private UserRepository userRepository;
 
     @Override
     public void execute(Long userId, String name) {
         User user = this.retrieveUserById.execute(userId);
         Preference preferenceToRemove = user.getPreferences()
                 .stream()
-                .filter(preference -> preference.equals(name))
+                .filter(preference -> preference.getPreferences().name().equals(name))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Preference to delete not found"));
         user.getPreferences().remove(preferenceToRemove);
 
-        this.preferenceRepository.delete(preferenceToRemove);
+        userRepository.save(user);
     }
 }
