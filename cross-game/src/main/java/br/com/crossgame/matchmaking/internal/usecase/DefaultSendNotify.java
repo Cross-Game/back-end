ae50df4361cb3a2354c334d0736f1ff6f863eb7a
Matheus <matheus.patricio@sptech.school>
2023-11-20 20:34:55 -0300

    add brasilia datetime

--- cross-game/src/main/java/br/com/crossgame/matchmaking/internal/usecase/DefaultSendNotify.java
+++ cross-game/src/main/java/br/com/crossgame/matchmaking/internal/usecase/DefaultSendNotify.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 @Service
@@ -40,12 +41,17 @@ public class DefaultSendNotify implements SendNotify {
        Notification notification1 = this.createNotification(notification,message,description,idUser,state);
        return NotificationBuildUtils.transform(notification1);
     }
-    private Notification createNotification(NotificationType notificationType, String message, String description,Long idUser,NotificationState state) {
+    private Notification createNotification(NotificationType notificationType, String message, String description, Long idUser, NotificationState state) {
         User user = userRepository.findById(idUser).orElseThrow(
-                () -> new ResponseStatusException(HttpStatus.NOT_FOUND,String.format("User with id %d not found!",idUser)));
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("User with id %d not found!", idUser)));
         this.addObserver(user);
-        Notification notify = new Notification(notificationType, message, description, LocalDateTime.now(),user,state);
-        notify.notifyObservers(notify,observers);
+
+        ZoneId brasiliaTimeZone = ZoneId.of("America/Sao_Paulo");
+        LocalDateTime nowInBrasilia = LocalDateTime.now(brasiliaTimeZone);
+
+        Notification notify = new Notification(notificationType, message, description, nowInBrasilia, user, state);
+        notify.notifyObservers(notify, observers);
         return notificationRepository.save(notify);
     }
+
 }
